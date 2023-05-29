a70c3d0d6b7d8ae49bfa2158b16146b783029b43
Shreedhar Raj <shreedharraj.buisness@gmail.com>
2023-05-29 22:07:52 +0530

    switched to vite

--- lib/fetchSuggestionFromChatGPT.ts
+++
@@ -1,7 +0,0 @@
-const fetchSuggestionFromChatGPT = () => {
-    fetch("/api/suggestion", {
-        cache: "no-store",
-    }).then((res) => res.json());
-}
-
-export default fetchSuggestionFromChatGPT;
